9648484ca8bb323609dd9daee21643b36102bfbc
allanvobraun <allanvobraun@gmail.com>
2020-03-15 14:30:43 -0300

    Added copy option

--- bin/index.js
+++ bin/index.js
@@ -5,16 +5,24 @@ const yargs = require("yargs"); // get cli args
 const boxen = require('boxen'); // prompt box
 const Confirm = require('prompt-confirm'); // confirmation lib
 const os = require("os");
+const clipboardy = require('clipboardy');
 const root = require("app-root-path");
 
 const DontPad = require(root + "/app/DontPad"); // dontpad class
 const user_name = os.userInfo().username; // os user name
 
 
-
 const options = yargs
 .scriptName("dontpad").usage("$0 <cmd> [args] or $0 'text' [args]")
-.command('get', 'Read content from a repository')
+.command('get', 'Read content from a repository', yargs => {
+  return yargs.option('c', {
+    alias: 'copy',
+    describe:'Copy the last line from the repository to the clipboard',
+    boolean: true,
+    type: 'boolean',
+    default: false
+  })
+})
 .option("r", {
   alias: "repository",
   describe: "Repository name in dontpad. \nExample: dontpad.com/repository",
@@ -43,9 +51,11 @@ let dontpad = new DontPad(options.repository);
 dontpad.separator = `\n${options.separator}\n`; //separador between each msg
 
 if (command === 'get') { // handles get command
+  
   boldMsg(`Lendo do repositório 'dontpad.com/${options.repository}'...`);
-    dontpad.read().then(res => {
+  dontpad.read().then(res => {
     console.log(boxen(res, {padding: 1, float:'center', borderStyle: 'round'}));
+    if (options.copy) copyLastLine(res);
   });  
   
 } else { // no command 
@@ -55,7 +65,7 @@ if (command === 'get') { // handles get command
     const prompt = new Confirm('Tem certeza que deseja sobreescrever todo o texto do repositório?');
     prompt.ask(answer => { // ask user confirmation
 
-      if (answer) { // if y
+      if (answer) { // if yes
         boldMsg('Sobreescrevendo o texto...');
         dontpad.write(command).then(res => {
           sucessMsg('sucesso!!!');
@@ -89,4 +99,12 @@ function sucessMsg(msg) {
   console.log(text);
 }
 
+function copyLastLine(text) {
+  const regex = /(.*$)/g; 
+  const last_line = regex.exec(text);
+  clipboardy.writeSync(last_line[1]);
+  boldMsg(`"${last_line[1]}" copied to clipboard`)
+  
+  
+}
 
