e3eb1cf70938e35c42ea0cb5c31af8ed9ec9c3e9
allanvobraun <allanvobraun@gmail.com>
2020-03-14 21:55:58 -0300

    começo refatoramento

--- app/DontPad.js
+++ app/DontPad.js
@@ -1,6 +1,6 @@
 
 const axios = require('axios');
-// const cheerio = require('cheerio');
+const cheerio = require('cheerio');
 
 module.exports = class DontPad {
     //propriedades e funções da classe aqui
@@ -15,5 +15,14 @@ module.exports = class DontPad {
         }
         let response = await axios.post(this.url, null, params);
         return response;
+    }
+
+    async read() {
+        let response = await axios(this.url).catch((err) => console.log(err));
+        const html = response.data;
+
+        const $ = cheerio.load(html);
+        const textarea = $('#text');
+        return textarea.html();
     }
 }
--- bin/index.js
+++ bin/index.js
@@ -3,36 +3,66 @@
 const chalk = require("chalk"); // biblioteca de estilzação de texto
 const yargs = require("yargs"); // pega args do cli
 const os = require("os");
-
 const root = require("app-root-path");
-const DontPad = require(root + "/app/DontPad"); // classe do dontpad
-var repository = "";
 
-// requer 1 argumento sem parametro
-// e 1 argumento -r
-const options = yargs.demandCommand(1).option("r", {
-  alias: "repository",
-  describe: "Repository name in dontpad. Example: dontpad.com/repository",
-  type: "string"
-}).argv;
 
-const user_name = os.userInfo().username; // nome do usuario do sistema
-const text = options._[0]; // pega o argumento
-const greeting = chalk.white.bold("Inserindo texto....");
+class Main {
+  constructor(params) {
+  const DontPad = require(root + "/app/DontPad"); // classe do dontpad
+  const user_name = os.userInfo().username; // nome do usuario do sistema
 
-console.log(greeting);
+  read(argv) {
+    dontpad.read().then(res => {
+    console.log(res);
+    
+  });
 
-// checa se o argumento foi passado
-if (typeof options.repository === 'undefined') {
-    repository = user_name;
 
-} else {
-    repository = options.repository;
+    
+  }
 }
 
-var dontpad = new DontPad(repository);
 
+function 
+
+  
+  
+}
+// requer 1 argumento sem parametro
+// e 1 argumento -r
+const options = yargs
+.scriptName("dontpad").usage('$0 <cmd> [args]')
+.command('get', 'Read content from a repository', () => {}, read)
+.option("r", {
+  alias: "repository",
+  describe: "Repository name in dontpad. \nExample: dontpad.com/repository",
+  type: "string",
+  default: user_name
+}).argv;
+
+
+
+// console.log(options);
+
+//const batata = yargs.scriptName("pirate-parser").usage('batatata').argv;
+
+const text = options._[0]; // pega o argumento sem parametro
+const greeting = chalk.white.bold("Inserindo texto....");
+console.log(greeting);
+
+
+var dontpad = new DontPad(options.repository);
+
+if (text) {
+  dontpad.write(text).then(res => {
+    console.log(res.config.url);
+  });
+} 
 // substitui todo o texto da pagina do dontpad
-dontpad.write(text).then(res => {
-  console.log(res.config.url);
-});
+
+
+// dontpad.read().then(res => {
+//   console.log(res);
+  
+// });
+
